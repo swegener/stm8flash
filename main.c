9d953b14a7ebcd29ec2365cd119972d90cd20751
Sven Wegener <sven.wegener@stealer.net>
2015-12-20 10:29:03 +0100

    Always claim usb interface
    Interacting with an unclaimed interface results in kernel messages.
    
    Signed-off-by: Sven Wegener <sven.wegener@stealer.net>

--- main.c
+++ main.c
@@ -102,10 +102,8 @@ bool usb_init(programmer_t *pgm, unsigned int vid, unsigned int pid) {
 		assert(r == 0);
 	}
 
-#if defined(__APPLE__) || defined(WIN32)
 	r = libusb_claim_interface(pgm->dev_handle, 0);
 	assert(r == 0);
-#endif
 
 	return(true);
 }
